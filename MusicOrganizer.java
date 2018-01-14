cf0f7e7e4d0cc1bdf8ef60f42e968e4bf9354b46
Yav <yavieron@hotmail.es>
2018-01-15 00:09:40 +0100

    Añadido método listAllFiles que muestra por pantalla todos los nombres de archivo de la colección

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -82,4 +82,13 @@ public class MusicOrganizer
     {
         player.stop();
     }
+    
+    /**
+     * Imprime por pantalla todos los nombres de archivos.
+     */
+    public void listAllFiles() {
+        for(String filename : files) {
+            System.out.println(filename);
+        }
+    }
 }
