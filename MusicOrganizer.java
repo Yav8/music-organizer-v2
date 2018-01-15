ad3f3f0bb3870a365fba9179aa9cc59509813614
Yav <yavieron@hotmail.es>
2018-01-15 17:08:25 +0100

    Creado método listMatching que muestra por pantalla los nombres de archivos de la colección que contengan el texto introducido por el usuario

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -13,7 +13,7 @@ public class MusicOrganizer
     private ArrayList<String> files;
     // A player for the music files.
     private MusicPlayer player;
-        
+
     /**
      * Create a MusicOrganizer
      */
@@ -22,7 +22,7 @@ public class MusicOrganizer
         files = new ArrayList<String>();
         player = new MusicPlayer();
     }
-    
+
     /**
      * Add a file to the collection.
      * @param filename The file to be added.
@@ -31,7 +31,7 @@ public class MusicOrganizer
     {
         files.add(filename);
     }
-    
+
     /**
      * Return the number of files in the collection.
      * @return The number of files in the collection.
@@ -40,7 +40,7 @@ public class MusicOrganizer
     {
         return files.size();
     }
-    
+
     /**
      * List a file from the collection.
      * @param index The index of the file to be listed.
@@ -52,7 +52,7 @@ public class MusicOrganizer
             System.out.println(filename);
         }
     }
-    
+
     /**
      * Remove a file from the collection.
      * @param index The index of the file to be removed.
@@ -82,17 +82,29 @@ public class MusicOrganizer
     {
         player.stop();
     }
-    
+
     /**
      * Imprime por pantalla todos los nombres de archivos
      * y también el índice que ocupa cada uno.
      */
     public void listAllFiles() {
         int position = 0;
-        
+
         for(String filename : files) {
             System.out.println(position + ": " + filename);
             position += 1;
+        }
+    }
+
+    /**
+     * Imprime por pantalla los nombres de archivos que contengan
+     * el texto introducido por parámetro.
+     */
+    public void listMatching(String searchString) {
+        for(String filename : files) {
+            if(filename.contains(searchString)) {
+                System.out.println(filename);
+            }
         }
     }
 }
